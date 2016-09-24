5bdde726c85b125c928e8b571574fc73a6fffc4e
Mike Rahnis <mike.rahnis@gmail.com>
2016-09-23 22:26:40 -0400

    tab linting

--- setup.py
+++ setup.py
@@ -14,29 +14,32 @@ with open('VERSION.txt', 'w') as fp:
     fp.write(version)
 
 setup(name='drapery',
-	version=version,
-	author='Michael Rahnis',
-	author_email='michael.rahnis@fandm.edu',
-	description='Python library and CLI tool to convert 2D geometries to 3D given an elevation source',
-	url='http://github.com/mrahnis/drapery',
-	license='BSD',
-	packages=find_packages(),
-	include_package_data=True,
-	install_requires=[
-		'shapely', 'rasterio', 'fiona', 'Click'
-	],
-	entry_points='''
-		[console_scripts]
-		drape=drapery.cli.drape:cli
-	''',
-	keywords='survey, conversion',
-	classifiers=[
-		'Development Status :: 3 - Alpha',
-		'License :: OSI Approved :: BSD License',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Topic :: Scientific/Engineering :: GIS'
-	]
+    version=version,
+    author='Michael Rahnis',
+    author_email='michael.rahnis@fandm.edu',
+    description='Python library and CLI tool to convert 2D geometries to 3D given an elevation source',
+    url='http://github.com/mrahnis/drapery',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'shapely',
+        'rasterio',
+        'fiona',
+        'click'
+    ],
+    entry_points='''
+        [console_scripts]
+        drape=drapery.cli.drape:cli
+    ''',
+    keywords='survey, conversion',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: GIS'
+    ]
 )
 
