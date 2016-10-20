bcfa6ba11e2c09b36e34e9800d68b95c4105cba9
Mike Rahnis <mike.rahnis@gmail.com>
2016-10-19 22:01:50 -0400

    change to Google style docstrings

--- drapery/ops/sample.py
+++ drapery/ops/sample.py
@@ -4,17 +4,14 @@ import logging
 from shapely.geometry import Point, LineString
 
 def drape(raster, feature):
-    """
-    Convert a 2D feature to a 3D feature by sampling a raster
+    """Convert a 2D feature to a 3D feature by sampling a raster
 
-    Parameters
-    ----------
-    raster (rasterio) - raster to provide the z coordinate
-    feature (dict) - fiona feature record to convert
+    Parameters:
+        raster (rasterio): raster to provide the z coordinate
+        feature (dict): fiona feature record to convert
 
-    Returns
-    -------
-    result (Point or Linestring) - shapely Point or LineString of xyz coordinate triples
+    Returns:
+        result (Point or Linestring): shapely Point or LineString of xyz coordinate triples
 
     """
     coords = feature['geometry']['coordinates']
@@ -33,17 +30,17 @@ def drape(raster, feature):
     return result
 
 def sample(raster, coords):
-    """
+    """Sample a raster at given coordinates
+
     Given a list of coordinates, return a list of x,y,z triples with z coordinates sampled from an input raster
 
-    Parameters
-    ----------
-    raster (rasterio) - raster dataset to sample
-    coords - array of tuples containing coordinate pairs (x,y) or triples (x,y,z)
+    Parameters:
+        raster (rasterio): raster dataset to sample
+        coords: array of tuples containing coordinate pairs (x,y) or triples (x,y,z)
 
-    Returns
-    -------
-    result - array of tuples containing coordinate triples (x,y,z)
+    Returns:
+        result: array of tuples containing coordinate triples (x,y,z)
+        
     """
     if len(coords[0]) == 3:
         logging.info('Input is a 3D geometry, z coordinate will be updated.')
