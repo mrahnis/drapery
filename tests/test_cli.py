546c4115456eccdc7cdd41408704041a1148882e
Mike Rahnis <mike.rahnis@gmail.com>
2017-04-27 12:08:36 -0400

    fix import

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,9 @@
 from click.testing import CliRunner
 
 import drapery
-from drapery.cli import drape
+from drapery.cli.drape import cli
 
-
-def test_err(tmpdir):
+def test_drape():
     runner = CliRunner()
-    result = runner.invoke(drape, ['--help'])
+    result = runner.invoke(cli, ['--help'])
     assert result.exit_code == 0
