2885492088bb3355f700f4841a2e5fa18f233210
Mike Rahnis <mike.rahnis@gmail.com>
2017-05-05 15:02:21 -0400

    add deploy to anaconda

---
+++ .ci/appveyor/deploy_to_anaconda.ps1
@@ -0,0 +1,19 @@
+if (($env:appveyor_repo_tag -eq "true") -and ($env:appveyor_repo_tag_name.StartsWith("v"))) {
+    write-output "Deploying to anaconda main channel..."
+    $channel = "main"
+} else {
+    write-output "Deploying to anaconda dev channel..."
+    $channel = "dev"
+    $env:BUILD_STR = "dev"
+}
+
+Invoke-Expression "conda config --set anaconda_upload no"
+
+$file_to_upload = (conda build --output .conda) | Out-String
+
+write-output "Uploading $file_to_upload..."
+Invoke-Expression "anaconda -t $env:ANACONDA_TOKEN upload --force --user mrahnis --channel $channel $file_to_upload"
+
+# Invoke-Expression "anaconda -t $env:ANACONDA_TOKEN upload --force --user landlab --channel $channel C:\\Miniconda\conda-bld\**\landlab*bz2"
+
+write-output "OK"
